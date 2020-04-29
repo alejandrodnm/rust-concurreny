ee6723eb71b59a7f025502d67e04af730a0f16d0
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2020-04-29 22:35:37 +0200

    Move the stack

---
+++ move-stack/src/main.rs
@@ -0,0 +1,41 @@
+#![feature(asm)]
+
+const SSIZE: isize = 48;
+
+#[derive(Debug, Default)]
+#[repr(C)]
+struct ThreadContext {
+    rsp: u64,
+}
+
+fn hello() -> ! {
+    println!("I LOVE WAKING UP ON A NEW STACK!");
+
+    loop {}
+}
+
+unsafe fn gt_switch(new: *const ThreadContext) {
+    println!("switch");
+    asm!("
+        mov 0x00($0), %rsp
+        ret
+        "
+        :
+        : "r"(new)
+        :
+        : "alignstack"
+    );
+}
+
+fn main() {
+    let mut ctx = ThreadContext::default();
+    let mut stack = vec![0_u8; SSIZE as usize];
+
+    unsafe {
+        let stack_bottom = stack.as_mut_ptr().offset(SSIZE);
+        let sb_aligned = (stack_bottom as usize & !15) as *mut u8;
+        std::ptr::write(sb_aligned.offset(-16) as *mut u64, hello as u64);
+        ctx.rsp = sb_aligned.offset(-16) as u64;
+        gt_switch(&mut ctx);
+    }
+}
